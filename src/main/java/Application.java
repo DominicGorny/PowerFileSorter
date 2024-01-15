9889f798f7502315fbd74bb4874fd6ae57525b91
DominicGorny <dominic.gorny11@gmail.com>
2024-01-15 17:12:40 +1300

    refactored folders for better naming

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,11 +8,13 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    public static String appVersion = "v0.1";
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("PowerSorter " + appVersion);
         stage.setScene(scene);
         stage.show();
     }
