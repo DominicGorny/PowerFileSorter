eaf87830e8e21a95b2c3153a5546c6d3727dc652
DominicGorny <dominic.gorny11@gmail.com>
2024-01-25 16:25:37 +1300

    built base structure to make an action chain

---
+++ src/main/java/com/example/powersorter/lowLevelActions/lowLevelAction.java
@@ -0,0 +1,14 @@
+package com.example.powersorter.lowLevelActions;
+
+/**
+ * interface that prescribes action functions for lowLevelActions.
+ */
+public interface lowLevelAction {
+
+    /**
+     * the action to perform when this function is run
+     * @return true/false success state
+     */
+    public boolean executeAction();
+
+}
